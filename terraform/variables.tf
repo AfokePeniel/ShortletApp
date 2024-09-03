1d1b6c6f998782142270ae598b20180861b85f79
AfokePeniel <afokekowho@gmail.com>
2024-09-03 18:58:45 -0400

    image tag

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,38 +14,7 @@ variable "terraform_service_account" {
   type        = string
 }
 
-variable "gke_cluster_name" {
-  description = "Name of the GKE cluster"
+variable "image_tag" {
+  description = "Tag for the time-api Docker image"
   type        = string
-  default     = "time-api-cluster"
-}
-
-variable "gke_num_nodes" {
-  description = "Number of nodes in the GKE cluster"
-  type        = number
-  default     = 1
-}
-
-variable "gke_machine_type" {
-  description = "Machine type for GKE nodes"
-  type        = string
-  default     = "e2-small"
-}
-
-variable "vpc_name" {
-  description = "Name of the VPC network"
-  type        = string
-  default     = "time-api-vpc"
-}
-
-variable "subnet_cidr" {
-  description = "CIDR range for the subnet"
-  type        = string
-  default     = "10.0.0.0/24"
-}
-
-variable "time_api_image" {
-  description = "Docker image for the Time API"
-  type        = string
-  default     = "gcr.io/PROJECT_ID/time-api:latest"
 }
