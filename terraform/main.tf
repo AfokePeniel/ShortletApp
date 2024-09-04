71ebdad7fe5d96759a848b0b603e16e38c386f24
AfokePeniel <afokekowho@gmail.com>
2024-09-04 11:03:48 -0400

    code change

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,9 @@
 # Enable required APIs
-resource "google_project_service" "artifact_registry" {
+resource "google_project_service" "apis" {
+  count   = 3
   project = var.project_id
-  service = "artifactregistry.googleapis.com"
-
+  service = ["artifactregistry.googleapis.com", "container.googleapis.com", "compute.googleapis.com"][count.index]
   disable_on_destroy = false
-}
-
-resource "google_project_service" "container" {
-  project = var.project_id
-  service = "container.googleapis.com"
-
-  disable_on_destroy = false
-}
-
-# Grant Artifact Registry Writer role to the service account
-resource "google_project_iam_member" "artifact_registry_writer" {
-  project = var.project_id
-  role    = "roles/artifactregistry.writer"
-  member  = "serviceAccount:${var.terraform_service_account}"
 }
 
 # VPC Network
@@ -32,47 +18,18 @@ resource "google_compute_subnetwork" "subnet" {
   ip_cidr_range = "10.0.0.0/24"
   region        = var.region
   network       = google_compute_network.vpc.id
+  private_ip_google_access = true
 }
 
-# NAT Gateway
-resource "google_compute_router" "router" {
-  name    = "time-api-router"
-  region  = var.region
-  network = google_compute_network.vpc.id
-}
-
-resource "google_compute_router_nat" "nat" {
-  name                               = "time-api-nat"
-  router                             = google_compute_router.router.name
-  region                             = var.region
-  nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-}
-
-# Firewall rules
+# Firewall rule to allow internal communication
 resource "google_compute_firewall" "allow_internal" {
   name    = "allow-internal"
   network = google_compute_network.vpc.name
-
   allow {
     protocol = "tcp"
     ports    = ["0-65535"]
   }
-
   source_ranges = ["10.0.0.0/24"]
-}
-
-resource "google_compute_firewall" "allow_health_check" {
-  name    = "allow-health-check"
-  network = google_compute_network.vpc.name
-
-  allow {
-    protocol = "tcp"
-    ports    = ["8080"]
-  }
-
-  source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
-  target_tags   = ["gke-node"]
 }
 
 # GKE Cluster
@@ -82,8 +39,8 @@ resource "google_container_cluster" "primary" {
   network  = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
 
-  remove_default_node_pool = true
   initial_node_count       = 1
+  remove_default_node_pool = true
 
   private_cluster_config {
     enable_private_nodes    = true
@@ -91,22 +48,9 @@ resource "google_container_cluster" "primary" {
     master_ipv4_cidr_block  = "172.16.0.0/28"
   }
 
-  ip_allocation_policy {
-    cluster_ipv4_cidr_block  = "/16"
-    services_ipv4_cidr_block = "/22"
-  }
+  ip_allocation_policy {}
 
-  master_authorized_networks_config {
-    cidr_blocks {
-      cidr_block   = "0.0.0.0/0"
-      display_name = "All"
-    }
-  }
-
-  depends_on = [
-    google_project_service.artifact_registry,
-    google_project_service.container
-  ]
+  depends_on = [google_project_service.apis]
 }
 
 resource "google_container_node_pool" "primary_nodes" {
@@ -118,12 +62,7 @@ resource "google_container_node_pool" "primary_nodes" {
   node_config {
     preemptible  = true
     machine_type = "e2-small"
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-
-    tags = ["gke-node"]
+    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 }
 
@@ -142,48 +81,23 @@ resource "kubernetes_deployment" "time_api" {
 
   spec {
     replicas = 2
-
     selector {
       match_labels = {
         app = "time-api"
       }
     }
-
     template {
       metadata {
         labels = {
           app = "time-api"
         }
       }
-
       spec {
         container {
           image = "gcr.io/${var.project_id}/time-api:${var.image_tag}"
           name  = "time-api"
-
           port {
             container_port = 8080
-          }
-
-          resources {
-            limits = {
-              cpu    = "0.5"
-              memory = "512Mi"
-            }
-            requests = {
-              cpu    = "250m"
-              memory = "50Mi"
-            }
-          }
-
-          liveness_probe {
-            http_get {
-              path = "/health"
-              port = 8080
-            }
-
-            initial_delay_seconds = 3
-            period_seconds        = 3
           }
         }
       }
@@ -204,26 +118,6 @@ resource "kubernetes_service" "time_api" {
       port        = 80
       target_port = 8080
     }
-
     type = "LoadBalancer"
-  }
-}
-
-# IAM role for GKE
-resource "google_project_iam_member" "gke_sa" {
-  project = var.project_id
-  role    = "roles/container.developer"
-  member  = "serviceAccount:${google_container_cluster.primary.service_account}"
-}
-
-# Policy as Code
-resource "google_project_organization_policy" "restrict_public_ip" {
-  project    = var.project_id
-  constraint = "compute.vmExternalIpAccess"
-
-  list_policy {
-    deny {
-      all = true
-    }
   }
 }
